be1047cd668083c831fa50b9f334cb641b74d80c
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-08-04 10:41:51 +0800

    Update autostart.sh

--- trunk/user/scripts/autostart.sh
+++ trunk/user/scripts/autostart.sh
@@ -45,7 +45,7 @@ logger -t "自动启动" "正在启动koolproxy"
 fi
 
 if [ $(nvram get aliddns_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动lucky工具箱"
+logger -t "自动启动" "正在启动阿里DDNS"
 /usr/bin/aliddns.sh start
 fi
 
